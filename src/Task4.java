9f5031990c9a4bebffef96ccb375d71062c4f71e
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-11-30 17:10:46 -0500

    Update Task4.java

--- src/Task4.java
+++ src/Task4.java
@@ -3,9 +3,12 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean continueYN = true;
+        double total = 0;
         do {
             double price = InputHelper.getRangedDouble(scan, "Please enter the price of your item: ", 0.5, 9.99);
             continueYN = InputHelper.getYNConfirm(scan, "Do you want to continue[Y/N]: ");
-        } while (continueYN == true);
+            total = total + price;
+        } while (continueYN);
+        System.out.printf("%-10s %5.2f", "Total cost in $:", total);
     }
 }
