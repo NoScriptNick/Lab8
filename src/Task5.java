086becb8d6b669f751558520cdfd2e19d91dbb5c
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-11-30 17:35:08 -0500

    Update Task5.java

--- src/Task5.java
+++ src/Task5.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
 public class Task5 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String ssn = InputHelper.getRegExString(scan, "Please enter your social security number: ", ("\\d{3}-\\d{2}-\\d{4}"));
+        System.out.println("Your ssn is " + ssn);
+        String uc = InputHelper.getRegExString(scan, "Please enter your UC Student M Number: ", ("(M|m)\\d{5}"));
+        System.out.println("Your Student M Number is: " + uc);
+        System.out.printf("%-8s %8s %8s %8s %8s", "Menu:", "O - Open", "S - Save", "V - View", "Q - Quit");
+        String menu = InputHelper.getRegExString(scan, " ", ("[OoSsVvQq]"));
+        System.out.println("You chose: " + menu);
+
+    }
 }
