dc17397b02e234dad72571cdd93705bdf7f80d38
NoScriptNick <144457708+NoScriptNick@users.noreply.github.com>
2023-12-01 22:52:40 -0500

    Update Task6.java

--- src/Task6.java
+++ src/Task6.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
 public class Task6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a message: ");
+        String msg = scan.nextLine();
+        InputHelper.prettyHeader(msg);
+    }
 }
